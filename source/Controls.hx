1d956496779f76b45d395f7d579aaea39ff19a27
Ethantobot <ethantobot7@gmail.com>
2026-05-09 17:04:52 +0200

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -450,6 +450,7 @@ class Controls extends FlxActionSet
 				case A_B | A_B_C | A_B_E | A_B_X_Y | A_B_C_X_Y | A_B_C_X_Y_Z | A_B_C_D_V_X_Y_Z:
 				inline forEachBound(Control.ACCEPT, (action, state) -> addButtonUI(action, VirtualPad.buttonA, state));
 				inline forEachBound(Control.BACK, (action, state) -> addButtonUI(action, VirtualPad.buttonB, state));
+				inline forEachBound(Control.PAUSE, (action, state) -> addButtonNOTES(action, VirtualPad.buttonX, state));
 				case NONE: // do nothing
 		    }
 		}
@@ -494,6 +495,7 @@ class Controls extends FlxActionSet
 				case A_B | A_B_C | A_B_E | A_B_X_Y | A_B_C_X_Y | A_B_C_X_Y_Z | A_B_C_D_V_X_Y_Z:
 				inline forEachBound(Control.ACCEPT, (action, state) -> addButtonNOTES(action, VirtualPad.buttonA, state));
 				inline forEachBound(Control.BACK, (action, state) -> addButtonNOTES(action, VirtualPad.buttonB, state));
+				inline forEachBound(Control.PAUSE, (action, state) -> addButtonNOTES(action, VirtualPad.buttonX, state));
 				case NONE: // do nothing
 			}
 		}
